76c0232a9cfed8157368fdc9fd7e72992a90812b
dvinueza21 <dvinueza21@gmail.com>
2026-02-08 19:29:38 +0100

    Fix AppServiceProvider https scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forzar HTTPS en producción (Railway)
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Prefetch Vite
         Vite::prefetch(concurrency: 3);
     }
 }
